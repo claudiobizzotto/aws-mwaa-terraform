ab6f0fa6a2646e2e4cd94767a645a56962838fb3
stelsemeyer <sebastian.telsemeyer@deliveryhero.com>
2021-05-05 18:27:48 +0200

    add dags user

--- main.tf
+++ main.tf
@@ -9,7 +9,7 @@ locals {
   }
 }
 
-resource "aws_iam_role" "iam_role" {
+resource "aws_iam_role" "airflow" {
   name = var.prefix
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
@@ -33,7 +33,7 @@ resource "aws_iam_role" "iam_role" {
   })
 }
 
-data "aws_iam_policy_document" "iam_policy_document" {
+data "aws_iam_policy_document" "airflow" {
   statement {
     sid       = ""
     actions   = ["airflow:PublishMetrics"]
@@ -129,21 +129,21 @@ data "aws_iam_policy_document" "iam_policy_document" {
   }
 }
 
-resource "aws_iam_policy" "iam_policy" {
+resource "aws_iam_policy" "airflow" {
   name   = var.prefix
   path   = "/"
-  policy = data.aws_iam_policy_document.iam_policy_document.json
+  policy = data.aws_iam_policy_document.airflow.json
 }
 
 resource "aws_iam_role_policy_attachment" "iam_role_policy_attachment" {
-  role       = aws_iam_role.iam_role.name
-  policy_arn = aws_iam_policy.iam_policy.arn
+  role       = aws_iam_role.airflow.name
+  policy_arn = aws_iam_policy.airflow.arn
 }
 
 resource "aws_mwaa_environment" "mwaa_environment" {
   source_bucket_arn     = aws_s3_bucket.s3_bucket.arn
   dag_s3_path           = "dags"
-  execution_role_arn    = aws_iam_role.iam_role.arn
+  execution_role_arn    = aws_iam_role.airflow.arn
   name                  = var.prefix
   max_workers           = var.mwaa_max_workers
   webserver_access_mode = "PUBLIC_ONLY"
@@ -179,4 +179,42 @@ resource "aws_mwaa_environment" "mwaa_environment" {
       log_level = "INFO"
     }
   }
+}
+
+# Access to push dags to S3:
+
+data "aws_iam_policy_document" "dags" {
+  statement {
+    sid = ""
+    actions = [
+      "s3:PutObject*",
+      "s3:GetBucket*",
+      "s3:List*"
+    ]
+    effect = "Allow"
+    resources = [
+      aws_s3_bucket.s3_bucket.arn,
+      "${aws_s3_bucket.s3_bucket.arn}/*"
+    ]
+  }
+}
+
+resource "aws_iam_user" "dags" {
+  name = var.prefix
+}
+
+resource "aws_iam_policy" "dags" {
+  name   = "${var.prefix}-dags"
+  path   = "/"
+  policy = data.aws_iam_policy_document.dags.json
+}
+
+resource "aws_iam_policy_attachment" "dags" {
+  name       = "${var.prefix}-dags"
+  users      = [aws_iam_user.dags.name]
+  policy_arn = aws_iam_policy.dags.arn
+}
+
+resource "aws_iam_access_key" "dags" {
+  user = aws_iam_user.dags.name
 }
--- outputs.tf
+++ outputs.tf
@@ -29,3 +29,23 @@ output "mwaa_webserver_url" {
   value       = aws_mwaa_environment.mwaa_environment.webserver_url
   description = "Webserver URL of MWAA environment."
 }
+
+output "region" {
+  value       = var.region
+  description = "The deployment region."
+}
+
+output "s3_bucket_name" {
+  value       = aws_s3_bucket.s3_bucket.bucket
+  description = "Name of S3 bucket."
+}
+
+output "access_key_id" {
+  value       = aws_iam_access_key.dags.id
+  description = "Access Key ID to push data to the dags bucket."
+}
+
+output "secret_access_key" {
+  value       = aws_iam_access_key.dags.secret
+  description = "Secret Access Key to push data to the dags bucket."
+}
